33def9f41f59802d6e4c5558928cd8ace5d53b9c
Anghelus Andrei <anghelusandrei2@gmail.com>
2022-12-19 13:03:13 +0200

    flood fill in progress

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,14 +8,20 @@ import org.opencv.videoio.VideoCapture;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionListener;
+import java.awt.desktop.UserSessionEvent;
+import java.awt.event.*;
 import java.io.*;
 import java.util.Objects;
 import java.awt.image.BufferedImage;
+import java.util.Vector;
+
+import static org.opencv.core.CvType.CV_8U;
 
 public class Main {
+
+    private static final int[] di = {1,0,-1,0};
+    private static final int[] dj = {0,1,0,-1};
+
     static BufferedImage Mat2BufferedImage(Mat matrix)throws Exception {
         MatOfByte mob=new MatOfByte();
         Imgcodecs.imencode(".jpg", matrix, mob);
@@ -24,6 +30,7 @@ public class Main {
         BufferedImage bi= ImageIO.read(new ByteArrayInputStream(ba));
         return bi;
     }
+
     public static void main(String[] args) {
         try {
                 System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
@@ -50,47 +57,69 @@ public class Main {
 
         System.out.println("Loaded OpenCV");
 
+
+        VideoCapture capture = new VideoCapture(0);
+
+        Mat matrix;
+        Mat image = new Mat();
+
         int[] layerSizes = new int[2];
         layerSizes[0] = 5;
         layerSizes[1] = 2;
 
+
+
         NeuralNetwork neuralNetwork = new NeuralNetwork(layerSizes);
 
-        VideoCapture capture = new VideoCapture(0);
-        Mat matrix = new Mat();
-        capture.read(matrix);
-        capture.release();
+        int[] trainingIndex = new int[2];
+        boolean training = false;
+
+
+        capture.read(image);
+
+        matrix = new Mat(image.rows(), image.cols(), image.type());
+
+        Core.copyTo(image, matrix, image);
+
+        Mat expectedImage = new Mat(image.rows(), image.cols(), CV_8U);
+        Mat expectedImage_rgb = new Mat(image.rows(), image.cols(), image.type());
+        Mat window_surface = new Mat(image.rows(), image.cols(), image.type());
+        Mat overlay = new Mat(image.rows(), image.cols(), CV_8U);
+        Mat overlay_rgb = new Mat(image.rows(), image.cols(), image.type());
+        Mat mask = new Mat();
+
         JFrame imageJframe = HighGui.createJFrame("Vision training data", JFrame.EXIT_ON_CLOSE);
         JLabel label = new JLabel();
-        label.addMouseListener(new MouseListener() {
+
+        label.addMouseListener(new MouseAdapter() {
             @Override
-            public void mouseClicked(MouseEvent e) {
+            public void mousePressed(MouseEvent arg0) {
+                if (arg0.getButton() == MouseEvent.BUTTON1){
+                    //left click
+                } else if (arg0.getButton() == MouseEvent.BUTTON2){
+                    //middle button
+                    Core.copyTo(image, matrix, image);
 
-            }
+                    overlay.setTo(new Scalar(0));
+                    expectedImage.setTo(new Scalar(0));
 
-            @Override
-            public void mousePressed(MouseEvent e) {
-            }
 
-            @Override
-            public void mouseReleased(MouseEvent e) {
-            }
-
-            @Override
-            public void mouseEntered(MouseEvent e) {
-
-            }
-
-            @Override
-            public void mouseExited(MouseEvent e) {
-
+                } else if (arg0.getButton() == MouseEvent.BUTTON3) {
+                    //right click
+                    Core.copyTo(overlay,mask,overlay);
+                    Core.copyTo(overlay,expectedImage,overlay);
+                    Core.copyMakeBorder(mask, mask, 1, 1, 1, 1, Core.BORDER_REPLICATE);
+                    System.out.println("pos: " + arg0.getX() + " & " + arg0.getY());
+                    Imgproc.floodFill(expectedImage, mask, new Point(arg0.getX(), arg0.getY()), new Scalar(255), new Rect(), new Scalar(0), new Scalar(0), 4 | Imgproc.FLOODFILL_MASK_ONLY | (255 << 8));
+                    Core.subtract(expectedImage, overlay, expectedImage);
+                }
             }
         });
-        label.addMouseMotionListener(new MouseMotionListener() {
+        label.addMouseMotionListener(new MouseMotionAdapter() {
             @Override
             public void mouseDragged(MouseEvent e) {
                 Point point = new Point(e.getX(), e.getY());
-                Imgproc.circle(matrix, point, 1, new Scalar(255,255,255),-1);
+                Imgproc.circle(overlay, point, 1, new Scalar(255),-1);
             }
 
             @Override
@@ -98,15 +127,28 @@ public class Main {
 
             }
         });
+
         try {
-            label.setIcon(new ImageIcon(Mat2BufferedImage(matrix)));
+            Core.copyTo(matrix, window_surface, matrix);
+            label.setIcon(new ImageIcon(Mat2BufferedImage(window_surface)));
 
             imageJframe.getContentPane().add(label);
             imageJframe.pack();
             imageJframe.setVisible(true);
-            while(true){
-                label.setIcon(new ImageIcon(Mat2BufferedImage(matrix)));
+
+
+            while(capture.read(image)){
+                Imgproc.cvtColor(overlay, overlay_rgb, Imgproc.COLOR_GRAY2RGB);
+                Imgproc.cvtColor(expectedImage, expectedImage_rgb, Imgproc.COLOR_GRAY2RGB);
+                Core.copyTo(matrix, window_surface, matrix);
+                Core.copyTo(overlay_rgb, window_surface, overlay);
+                Core.copyTo(expectedImage_rgb, window_surface, expectedImage);
+
+                label.setIcon(new ImageIcon(Mat2BufferedImage(window_surface)));
             }
+
+            capture.release();
+
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
