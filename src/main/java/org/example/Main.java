0456b9fd6a25c05b1927a1922b8f2288920e12f7
Anghelus Andrei <anghelusandrei2@gmail.com>
2022-12-19 20:30:26 +0200

    fill + neural network

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -22,6 +22,38 @@ public class Main {
     private static final int[] di = {1,0,-1,0};
     private static final int[] dj = {0,1,0,-1};
 
+    private static void Learn(Mat data, Mat expectedOutput, NeuralNetwork network){
+        for(int i = 0; i < data.rows(); i++){
+            for(int j = 0; j < data.cols(); j++){
+                double[] inputs = data.get(i,j);
+                inputs[0] /= 255;
+                inputs[1] /= 255;
+                inputs[2] /= 255;
+                double[] expectedOutputs = expectedOutput.get(i,j);
+                expectedOutputs[0] /= 255;
+                expectedOutputs[1] = 1-expectedOutputs[0];
+                network.Learn(inputs, expectedOutputs, 0.2);
+            }
+        }
+    }
+
+    private static double[][][] lookupTable(NeuralNetwork network){
+        double[][][] table = new double[255][255][255];
+        for(int i = 0; i < 255; i++){
+            for(int j = 0; j < 255; j++){
+                for(int k = 0; k < 255; k++){
+                    double[] inputs = new double[3];
+                    inputs[0] = i;
+                    inputs[1] = j;
+                    inputs[2] = k;
+                    double[] outputs = network.CalculateOutputs(inputs);
+                    table[i][j][k] = outputs[0];
+                }
+            }
+        }
+        return table;
+    }
+
     static BufferedImage Mat2BufferedImage(Mat matrix)throws Exception {
         MatOfByte mob=new MatOfByte();
         Imgcodecs.imencode(".jpg", matrix, mob);
@@ -29,6 +61,22 @@ public class Main {
 
         BufferedImage bi= ImageIO.read(new ByteArrayInputStream(ba));
         return bi;
+    }
+
+    private static void FloodFill(int i, int j, Mat matToFill, Mat matToDetect, Scalar valueToFill, Scalar valueToDetect){
+        if(i>=0 && i<matToDetect.rows() && j>=0 && j<matToDetect.cols()){
+            Scalar value1 = new Scalar(0);
+            Scalar value2 = new Scalar(0);
+            value1.val = matToDetect.get(i,j);
+            value2.val = matToFill.get(i,j);
+
+            if(value1.val[0] != valueToDetect.val[0] && value2.val[0] != valueToDetect.val[0]){
+                matToFill.put(i,j, valueToFill.val);
+                for(int k = 0; k < 4; k++){
+                    FloodFill(i + di[k], j + dj[k], matToFill, matToDetect, valueToFill, valueToDetect);
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
@@ -106,12 +154,17 @@ public class Main {
 
                 } else if (arg0.getButton() == MouseEvent.BUTTON3) {
                     //right click
-                    Core.copyTo(overlay,mask,overlay);
+
+                    FloodFill(arg0.getY(), arg0.getX(), expectedImage, overlay, new Scalar(255), new Scalar(255));//fara stack sau alte chestii de optimizare
+
+                    //openCv method:
+                    /*Core.copyTo(overlay,mask,overlay);
                     Core.copyTo(overlay,expectedImage,overlay);
                     Core.copyMakeBorder(mask, mask, 1, 1, 1, 1, Core.BORDER_REPLICATE);
                     System.out.println("pos: " + arg0.getX() + " & " + arg0.getY());
                     Imgproc.floodFill(expectedImage, mask, new Point(arg0.getX(), arg0.getY()), new Scalar(255), new Rect(), new Scalar(0), new Scalar(0), 4 | Imgproc.FLOODFILL_MASK_ONLY | (255 << 8));
                     Core.subtract(expectedImage, overlay, expectedImage);
+                     */
                 }
             }
         });
